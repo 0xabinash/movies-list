1ad20fb8a3d635fdaf78ee8ed9500dbc4c56619a
Abinash Yadav <avinashy38@gmail.com>
2023-11-20 21:03:46 +0530

    second push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import { Home } from "./components/exporter";
 
 function App() {
   return (
     <div className="App">
-      app
+      <Home />
     </div>
   );
 }
